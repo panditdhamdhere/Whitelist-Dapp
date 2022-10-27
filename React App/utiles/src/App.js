1a2bf947fc4071abb61a0d5674bc93d835725058
pandit <dhamdherepandit@gmail.com>
2022-10-27 15:29:46 +0530

    utiles app

---
+++ React App/utiles/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Navbar from './components/Navbar'
+function App() {
+  return (
+    <>
+      <Navbar title="TextUtils" about="aboutText"/>
+    </>
+  );
+}
+
+export default App;
